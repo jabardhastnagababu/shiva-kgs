a9ac6d54595cb9284ce4486c78099f4c88d1dcc5
SudigaliSudheer <putnalasrikarguptha@gmail.com>
2026-09-25 14:29:47 +0530

    Create build.gradle.kts

---
+++ .github/workflows/core/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
